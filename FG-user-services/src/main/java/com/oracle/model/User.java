000d8b9b84da0f2722bb8f2d57f932aaa165bbd9
Aayushi <aayushi.sinha1980@gmail.com>
2025-09-10 12:58:05 +0530

    added advisor-dashboard charts

--- FG-user-services/src/main/java/com/oracle/model/User.java
+++ FG-user-services/src/main/java/com/oracle/model/User.java
@@ -2,6 +2,8 @@ package com.oracle.model;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
@@ -34,5 +36,6 @@ public class User {
     
     @Column
     private float currentBalance;
+
 }
 
