9892e7b284ac9157ce9136c6b33ef7a23e9edc88
unknown <Rijutha>
2025-09-11 15:16:33 +0530

    sign up page angular

--- angular_frontend/src/app/auth/auth.ts
+++ angular_frontend/src/app/auth/auth.ts
@@ -13,5 +13,16 @@ export class Auth {
   login(username: string, password: string): Observable<any> {
     return this.http.post(`${this.apiUrl}/login`, { username, password });
   }
-  
+  signup(signupData: { username: string; email: string; password: string }): Observable<any> {
+  const payload = {
+    username: signupData.username,
+    email: signupData.email,
+    password: signupData.password,
+    roleId: 'user',            // default role
+    currentBalance: 0.0          // default balance
+  };
+
+  return this.http.post(`http://localhost:8060/users`, payload);
+}
+
 }
--- angular_frontend/src/app/auth/signup/signup.ts
+++ angular_frontend/src/app/auth/signup/signup.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { Auth } from '../auth';
 
 @Component({
   selector: 'app-signup',
@@ -7,5 +9,28 @@ import { Component } from '@angular/core';
   styleUrl: './signup.css'
 })
 export class Signup {
+  signupData = {
+    username: '',
+    email: '',
+    password: ''
+  };
+
+  errorMessage = '';
+
+  constructor(private auth: Auth, private router: Router) {}
+
+  onSignup() {
+    this.auth.signup(this.signupData).subscribe({
+      next: (response) => {
+        console.log('Signup success:', response);
+        alert('Signup successful! Please login.');
+        this.router.navigate(['/login']);
+      },
+      error: (err) => {
+        console.error('Signup failed:', err);
+        this.errorMessage = 'Signup failed. Try again.';
+      }
+    });
+  }
 
 }
